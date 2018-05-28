f9c1f24ce9e167f912dfd3bdc602cf973cdf7e72
NOOOOOOO0000000b <39006590+NOOOOOOO0000000b@users.noreply.github.com>
2018-05-28 13:53:56 -0400

    for proxy sites

--- background.js
+++ background.js
@@ -3,5 +3,5 @@
 // Called when the user clicks on the extension
 chrome.browserAction.onClicked.addListener(function(tab) {
   // open surviv.io in a new tab
-  chrome.tabs.create({"url": "http://surviv.io"});
+  chrome.tabs.create({"url": "http://2dbattleroyale.com"});
 });
